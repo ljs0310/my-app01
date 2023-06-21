56e129275bf7d4cf2c40bd31a6e8107ff75250c3
ljs03101@gmail.com <ljs03101@gmail.com>
2023-06-21 10:07:55 +0900

    주석달기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Fragment } from "react";
 
 function App() {
+  //주석
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          다른 이름의 제목
-        </a>
-      </header>
-    </div>
+    //주석
+    <>
+      <div className="App">
+        {/* // 여러줄 주석
+        주석
+        주석
+        
+      */}
+
+        <h1>Start React</h1>
+      </div>
+    </>
   );
 }
 
